a2f3df64a229b04303c7ff27fc2c4f0853c7d29f
Frenck Lutke <git@innologi.nl>
2017-12-22 18:47:25 +0100

    [TASK] Remove remaining references to removed controller actions
    Change-Id: Iec8d03bb4242d593d524c15c8bda3827e1ba8b99

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,14 +5,14 @@ defined('TYPO3_MODE') or die();
 	'Innologi.' . $_EXTKEY,
 	'Filelist',
 	array(
-		'Category' => 'list, sort, new, create, edit, update, delete, ajaxVerifyToken, ajaxGenerateTokens',
+		'Category' => 'list, sort, new, create, edit, update, delete',
 		'File' => 'list, sort, download, new, create, edit, update, delete, search',
 		'Link' => 'new, create, edit, update, delete',
 	),
 	// non-cacheable actions
 	array(
 		// we don't cache new because feUser-id might be used in token generation
-		'Category' => 'new, sort, create, update, delete, ajaxVerifyToken, ajaxGenerateTokens',
+		'Category' => 'new, sort, create, update, delete',
 		// we don't cache list because of the category owner permissions (quick workaround)
 		'File' => 'list, sort, download, create, update, delete, search',
 		'Link' => 'create, update, delete',
